130df38d7eea7aa728b97ad4cbcfe69dcc43c59c
Dhruv Maheshwari <dhwari@gmail.com>
2013-11-16 17:31:59 -0500

    added db capabilities

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask, jsonify, Response, request, json
+from flask import Flask, g, jsonify, Response, request, json, render_template
+import redis
+import os
 from penn.registrar import Registrar
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    g.db= redis.StrictRedis(host="localhost", port = 6379)
+
+
+@app.route('/')
+def form():
+    return render_template("form.html")
+
+
+@app.route('/addnumber', methods= ['POST'])
+def add_number():
+    add_info = request.form
+    number  = add_info["number"]
+    print number
+    course = add_info["course"]
+    print course
+    g.db.sadd(course, number)
+    return jsonify({"status" : "okay",
+            "number": number, "course": course})
+
+@app.route('/getnumbers/<string:course_id>')
+def listNumbersForClass(course_id):
+    setNumbers = g.db.smembers(course_id)
+    for x in setNumbers:
+        print x
+    return jsonify({"set": setNumbers})
+
 
 
 @app.route('/course/<string:course_id>')
